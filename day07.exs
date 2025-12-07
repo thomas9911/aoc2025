0a237923c767ca10c48fdae5a300bda34ba10d82
Thomas Timmer <thomastimmer11@hotmail.com>
2025-12-07 11:53:59 +0100

    chore: cleanup day 7 a bit

--- day07.exs
+++ day07.exs
@@ -46,7 +46,7 @@ print_schema = fn schema, schematic ->
   IO.puts("")
 end
 
-a = fn ->
+shared = fn ->
   {starting, schematic_map} = parse.() |> Map.split_with(fn {_, value} -> value == :start end)
 
   [starting_point] = Map.keys(starting)
@@ -57,22 +57,22 @@ a = fn ->
 
   path =
     0..max_height
-    |> Enum.reduce(%{starting_point => true}, fn y, acc ->
-      Enum.reduce(0..max_width, acc, fn x, acc ->
-        hit_above? = Map.has_key?(acc, {x, y - 1})
+    |> Enum.reduce(%{starting_point => 1}, fn y, acc_root ->
+      Enum.reduce(0..max_width, acc_root, fn x, acc ->
+        hit_above_count = Map.get(acc, {x, y - 1}, 0)
 
         cond do
-          MapSet.member?(schematic, {x, y}) and hit_above? ->
+          MapSet.member?(schematic, {x, y}) and hit_above_count > 0 ->
             # passed splitter
             acc
-            |> Map.put({x + 1, y}, true)
-            |> Map.put({x - 1, y}, true)
+            |> Map.update({x + 1, y}, hit_above_count, &(&1 + hit_above_count))
+            |> Map.update({x - 1, y}, hit_above_count, &(&1 + hit_above_count))
             |> Map.update(:split_point, [{x, y}], &[{x, y} | &1])
 
-          hit_above? ->
+          hit_above_count > 0 ->
             # just pass it down
             acc
-            |> Map.put({x, y}, true)
+            |> Map.update({x, y}, hit_above_count, &(&1 + hit_above_count))
 
           true ->
             acc
@@ -82,8 +82,12 @@ a = fn ->
 
   {split_point, path} = Map.pop(path, :split_point)
 
-  _ = path
-  # print_schema.(path)
+  {path, split_point, max_height}
+end
+
+a = fn ->
+  {_, split_point, _} = shared.()
+
   length(split_point)
 end
 
@@ -150,39 +154,10 @@ b = fn ->
   # a splitter always flips it to another parity
   # use hint from reddit
   #
+  # thinking way too dificult
+  #
   # try: https://i.redd.it/9lxea8kv9q5g1.gif
-
-  {starting, schematic_map} = parse.() |> Map.split_with(fn {_, value} -> value == :start end)
-
-  [starting_point] = Map.keys(starting)
-  schematic = schematic_map |> Map.keys() |> MapSet.new()
-
-  {max_width, max_height} =
-    Enum.reduce(schematic, {0, 0}, fn {x, y}, {max_x, max_y} -> {max(max_x, x), max(max_y, y)} end)
-
-  path =
-    0..max_height
-    |> Enum.reduce(%{starting_point => 1}, fn y, acc_root ->
-      Enum.reduce(0..max_width, acc_root, fn x, acc ->
-        hit_above_count = Map.get(acc, {x, y - 1}, 0)
-
-        cond do
-          MapSet.member?(schematic, {x, y}) and hit_above_count > 0 ->
-            # passed splitter
-            acc
-            |> Map.update({x + 1, y}, hit_above_count, &(&1 + hit_above_count))
-            |> Map.update({x - 1, y}, hit_above_count, &(&1 + hit_above_count))
-
-          hit_above_count > 0 ->
-            # just pass it down
-            acc
-            |> Map.update({x, y}, hit_above_count, &(&1 + hit_above_count))
-
-          true ->
-            acc
-        end
-      end)
-    end)
+  {path, _, max_height} = shared.()
 
   path
   |> Map.filter(fn {{_, y}, _} -> y == max_height end)
